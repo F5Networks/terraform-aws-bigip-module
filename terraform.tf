e41a0487bc6a73f8f024c14117ce74365c02b3f3
Sebastian Möhn <smoehn@tecracer.de>
2022-02-02 20:34:21 +0100

    Removed old Template Provider (deprecated), Introduced locals/data/terraform files for a best-practice structure

---
+++ terraform.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">3.8.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">2.3.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">2.1.2"
+    }
+  }
+}
